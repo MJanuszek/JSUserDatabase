58ae7000b6844c67bc3afd25a2f82d3a9931751f
MJanuszek <monika.januszek.mail@gmail.com>
2022-11-17 14:30:14 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+usersData = [];
+
+// --------------------------
+// from HTML
+
+let inputName = document.querySelector(".name");
+let inputSurname = document.querySelector(".surname");
+const submitBtn = document.querySelector(".submit");
+
+// ----------------------------
+let userName = "";
+let userSurname = "";
+let userId = 0;
+// ----------------------------
+
+const addUserName = function() {
+    // extract id:
+    for(let i =0; i<usersData.length; i++){
+        userId = usersData.length;
+    }
+    // create new user object and add to array:
+    userName = inputName.value;
+    userSurname = inputSurname.value;
+    usersData.push({name:userName, surname: userSurname, id:userId })
+
+};
+// ---------------
+
+
+// -------------------------------
+const addNewUser = function() {
+   addUserName();
+   console.log(userName, userSurname)
+   inputName.value = "";
+   inputSurname.value = "";
+};
+
+submitBtn.addEventListener("click", addNewUser)
